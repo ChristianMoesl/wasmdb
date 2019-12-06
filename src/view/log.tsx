f83152f213c6a86b62708023bea15d7a6cf1e802
Christian Mösl <christian.moesl@live.at>
2019-12-06 13:44:35 +0100

    Reduce distance between log messages

--- src/view/log.tsx
+++ src/view/log.tsx
@@ -2,16 +2,36 @@ import React, {useRef, useEffect} from 'react'
 import {List} from "immutable"
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
 import {
-  ExpansionPanelSummary, 
-  ExpansionPanelDetails,
-  ExpansionPanel,
-  Typography,
   List as ListContainer,
   ListItem,
   Box,
 } from '@material-ui/core'
-import {ExpandMore} from '@material-ui/icons'
 
+export type LogMessage = {
+  date: string,
+  msg: string,
+  error: boolean,
+}
+
+const useEntryStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    item: {
+      padding: theme.spacing(0.5),
+    },
+  }),
+)
+
+function LogEntry(props: {message: LogMessage}) {
+  const classes = useEntryStyles()
+  return ( 
+    <ListItem className={classes.item}>
+      <Box display="flex">
+        <Box fontFamily="Monospace" fontWeight="fontWeightMedium" color={props.message.error ? "text.error" : "text.primary"}>{props.message.date}</Box>
+        <Box fontFamily="Monospace" fontWeight="fontWeightMedium" color={props.message.error ? "text.error" : "text.primary"}>{props.message.msg}</Box>
+      </Box>
+    </ListItem>
+  )
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,29 +44,8 @@ const useStyles = makeStyles((theme: Theme) =>
       fontSize: theme.typography.pxToRem(15),
       fontWeight: theme.typography.fontWeightRegular,
     },
-    container: {
-    },
   }),
 )
-
-export type LogMessage = {
-  date: string,
-  msg: string,
-  error: boolean,
-}
-
-function LogEntry(props: {message: LogMessage}) {
-  return ( 
-    <ListItem>
-      <Typography color={props.message.error ? "error" : "primary"}>
-        <Box display="flex">
-          <Box fontFamily="Monospace" fontWeight="fontWeightMedium">{props.message.date}</Box>
-          <Box fontFamily="Monospace" fontWeight="fontWeightMedium">{props.message.msg}</Box>
-        </Box>
-      </Typography>
-    </ListItem>
-  )
-}
 
 export interface Props {
   title: string,
@@ -63,7 +62,7 @@ export function Log(props: Props) {
 
   return (
     <Box component="div" className={classes.root}>
-      <ListContainer className={classes.container}>
+      <ListContainer>
         {props.messages.map((m, i) => <LogEntry key={i} message={m} />)}
         <ListItem component="div" ref={logEndRef} />
       </ListContainer>
